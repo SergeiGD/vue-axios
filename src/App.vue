43cc0ef77b4e847c659f3435b463650c2289aa7c
Sergei Golovin <sergeigolovin.dm@gmail.com>
2023-06-10 22:36:11 +0800

    nav edit

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="container-xl d-flex flex-column gap-5">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/tags">Tags</router-link>
+  <div class="container-xl d-flex flex-column gap-5 ">
+    <nav class="d-flex gap-3 justify-content-center p-3 fw-bold">
+      <router-link 
+        :to="{ name: 'Home' }"
+        :class="[this.$route.fullPath === '/' ? 'text-warning' : 'text-secondary']"
+      >Home</router-link>
+
+      <router-link 
+        :to="{ name: 'Tags' }" 
+        :class="[/^\/tags(\w*)/.test(this.$route.fullPath) ? 'text-warning' : 'text-secondary']"
+      >Tags</router-link>
+
+      <router-link 
+        v-if="!userStore.isAuthenticated" 
+        :to="{ name: 'Login' }"
+        :class="[ /^\/auth(\w*)/.test(this.$route.fullPath) ? 'text-warning' : 'text-secondary']"
+      >Login</router-link>
+
+      <a v-if="userStore.isAuthenticated" href="#" @click="logout">Logout</a>
+      
     </nav>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { mapStores } from "pinia";
+import { useUserStore } from '@/stores/user'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default({
+  computed: {
+    ...mapStores(useUserStore)
+  },
+  methods: {
+    logout() {
+      this.userStore.logOut();
+      this.$router.push({name: "Tags"});
     }
   }
-}
-</style>
+})
+</script>
